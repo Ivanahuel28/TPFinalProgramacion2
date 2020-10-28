4414bd2cf7836320cfe40e2a56dce186b9edd299
Ivan <ivanahuel.ing@gmail.com>
2020-10-28 16:20:15 -0300

    agregue los comentarios a las funciones

--- pila.c
+++ pila.c
@@ -1,10 +1,24 @@
 #include "pila.h"
 
+/*********************************************************//**
+*
+* \brief Inicializa la pila recibida
+* \param puntero a pila
+* \return void
+*
+***********************************************************/
 void inicPila(pila * p)
 {
     (*p) = inicLista();
 }
 
+/*********************************************************//**
+*
+* \brief Comprueba que la pila este vacia
+* \param pila como copia
+* \return int flag - 1 si esta vacia - 0 si tiene al menos un elemento
+*
+***********************************************************/
 int pilaVacia(pila p)
 {
     int flag = 1;
@@ -17,11 +31,26 @@ int pilaVacia(pila p)
     return flag;
 }
 
+/*********************************************************//**
+*
+* \brief Coloca el elemento recibido al tope de la pila
+* \param puntero a la pila
+* \param dato a apilar
+* \return void
+*
+***********************************************************/
 void apilar(pila * p,  dato) /// falta el tipo de dato recibido
 {
     agregarAlPrincipio(p,crearNodo(dato));
 }
 
+/*********************************************************//**
+*
+* \brief Quita el elemento superior de la pila y lo retorna
+* \param puntero a la pila
+* \return dato - dato que se desapilo
+*
+***********************************************************/
  despilar(pila * p) /// falta el tipo de dato a retornar
 {
     aux; /// falta el tipo de dato del auxiliar
@@ -33,11 +62,25 @@ void apilar(pila * p,  dato) /// falta el tipo de dato recibido
     return aux;
 }
 
+/*********************************************************//**
+*
+* \brief Retorna el elemento superior de la pila
+* \param pila como copia
+* \return dato - dato superior de la pila
+*
+***********************************************************/
  tope(pila p) /// falta el tipo de dato a retornar
 {
     return p->dato;
 }
 
+/*********************************************************//**
+*
+* \brief Muestra todo el contenido de la pila
+* \param pila como copia
+* \return void
+*
+***********************************************************/
 void mostrarPila(pila p)
 {
     pila aux;
